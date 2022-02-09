899dab810c6f50d69bd7e902957626e31da75ef8
OmarAhmed3012 <omaraboshamaa@gmail.com>
2022-02-10 00:42:42 +0200

    Login Form & test req from the client

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,12 +15,31 @@ export const Register = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
     if (password !== password2) {
       console.log('Paswords not match')
     } else {
-      console.log(formData)
+      console.log('success')
+      //   const newUser = {
+      //     name,
+      //     email,
+      //     password,
+      //   }
+      //   try {
+      //     const config = {
+      //       headers: {
+      //         'Content-type': 'application/json',
+      //       },
+      //     }
+
+      //     const body = JSON.stringify(newUser)
+
+      //     const res = await axios.post('/api/users', body, config)
+      //     console.log(res.data)
+      //   } catch (err) {
+      //     console.log(err.response.data)
+      //   }
     }
   }
 
@@ -87,7 +107,7 @@ export const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </section>
   )
