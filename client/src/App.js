9a62f2da61cc5066e4b5fec643a545fa3552b624
OmarAhmed3012 <omaraboshamaa@gmail.com>
2022-03-22 15:46:36 +0200

    Load User & Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -8,19 +8,31 @@ import Alert from './components/layout/Alert'
 
 import { Provider } from 'react-redux'
 import store from './store'
+import setAuthToken from './utils/setAuthToken'
+import { loadUser } from './actions/auth'
+import { useEffect } from 'react'
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Navbar />
-      <Alert />
-      <Routes>
-        <Route exact path='/' element={<Landing />} />
-        <Route exact path='register' element={<Register />} />
-        <Route exact path='login' element={<Login />} />
-      </Routes>
-    </Router>
-  </Provider>
-)
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='register' element={<Register />} />
+          <Route exact path='login' element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+  )
+}
 
 export default App
