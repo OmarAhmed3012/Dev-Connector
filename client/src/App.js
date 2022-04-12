4d6cf77b14dc184d227ad3e8f5d7366e87c39ac0
OmarAhmed3012 <omaraboshamaa@gmail.com>
2022-04-12 14:27:37 +0200

    Starting in Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './components/layout/Navbar'
-import { Landing } from './components/layout/Landing'
+import Landing from './components/layout/Landing'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
